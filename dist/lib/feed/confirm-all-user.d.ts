7b5c36fa92e1d2c4693cd8ccd0d0dda705f5218e
outranker <outranker222@gmail.com>
2022-02-18 18:08:35 +0900

    republish

--- dist/lib/feed/confirm-all-user.d.ts
+++
@@ -1,1 +0,0 @@
-export default function (params: string): Promise<void>;
--- dist/lib/feed/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { default as confirmUser } from "./confirm-user";
-export { default as confirmUserAll } from "./confirm-all-user";
