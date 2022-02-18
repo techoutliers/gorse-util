da4e2f004086e7bf799ee128872ce8037cec401b
outranker <outranker222@gmail.com>
2022-02-18 18:03:07 +0900

    set targer to es5 in tsconfig

--- dist/index.js
+++ dist/index.js
@@ -3,5 +3,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const lib_1 = __importDefault(require("./lib"));
+var lib_1 = __importDefault(require("./lib"));
 exports.default = new lib_1.default();
--- dist/lib/index.js
+++ dist/lib/index.js
@@ -8,65 +8,121 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const axios_1 = __importDefault(require("axios"));
-class GorseUtil {
-    constructor() {
+var axios_1 = __importDefault(require("axios"));
+var GorseUtil = /** @class */ (function () {
+    function GorseUtil() {
         this.feedUrl = null;
         this.directoryUrl = null;
         this.logger = undefined;
     }
-    init(args) {
+    GorseUtil.prototype.init = function (args) {
         this.feedUrl = args.feedUrl;
         this.directoryUrl = args.directoryUrl;
         this.logger = args.logger;
-    }
-    feed_deleteAVibe(args) {
+    };
+    GorseUtil.prototype.feed_deleteAVibe = function (args) {
         var _a;
-        return __awaiter(this, void 0, void 0, function* () {
-            (_a = this.logger) === null || _a === void 0 ? void 0 : _a.info({ args }, "feed_deleteAVibe");
-            const { vibeId, likedByUserIds } = args;
-            yield axios_1.default.delete(this.feedUrl + `item/${vibeId}`).catch((e) => {
-                var _a;
-                (_a = this.logger) === null || _a === void 0 ? void 0 : _a.error({ err: e }, "failed to delete vibe from gorse feed");
-            });
-            for (let i = 0; i < likedByUserIds.length; i++) {
-                const result = yield axios_1.default
-                    .delete(this.feedUrl + `feedback/${likedByUserIds[i]}/${vibeId}`)
-                    .catch((e) => {
-                    var _a;
-                    (_a = this.logger) === null || _a === void 0 ? void 0 : _a.error({ err: e }, "failed to delete vibe feedback from gorse feed");
-                });
-            }
-        });
-    }
-    feed_toggleSecretUser(args) {
-        var _a;
-        return __awaiter(this, void 0, void 0, function* () {
-            (_a = this.logger) === null || _a === void 0 ? void 0 : _a.info({ args }, "feed_toggleSecretUser");
-            const { vibes, status } = args;
-            const items = [];
-            for (let i = 0; i < vibes.length; i++) {
-                const item = {
-                    itemid: vibes[i]._id,
-                    status: status,
-                    adminstatus: "",
-                };
-                items.push(item);
-            }
-            const headers = { "Content-Type": "application/json" };
-            const result = yield axios_1.default
-                .patch(this.feedUrl + `item`, items, {
-                headers: headers,
-            })
-                .catch((err) => {
-                var _a;
-                (_a = this.logger) === null || _a === void 0 ? void 0 : _a.error({ err: err }, `failed to patch vibe status to ${status} in gorse feed`);
+        return __awaiter(this, void 0, void 0, function () {
+            var vibeId, likedByUserIds, i, result;
+            var _this = this;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0:
+                        (_a = this.logger) === null || _a === void 0 ? void 0 : _a.info({ args: args }, "feed_deleteAVibe");
+                        vibeId = args.vibeId, likedByUserIds = args.likedByUserIds;
+                        return [4 /*yield*/, axios_1.default.delete(this.feedUrl + "item/".concat(vibeId)).catch(function (e) {
+                                var _a;
+                                (_a = _this.logger) === null || _a === void 0 ? void 0 : _a.error({ err: e }, "failed to delete vibe from gorse feed");
+                            })];
+                    case 1:
+                        _b.sent();
+                        i = 0;
+                        _b.label = 2;
+                    case 2:
+                        if (!(i < likedByUserIds.length)) return [3 /*break*/, 5];
+                        return [4 /*yield*/, axios_1.default
+                                .delete(this.feedUrl + "feedback/".concat(likedByUserIds[i], "/").concat(vibeId))
+                                .catch(function (e) {
+                                var _a;
+                                (_a = _this.logger) === null || _a === void 0 ? void 0 : _a.error({ err: e }, "failed to delete vibe feedback from gorse feed");
+                            })];
+                    case 3:
+                        result = _b.sent();
+                        _b.label = 4;
+                    case 4:
+                        i++;
+                        return [3 /*break*/, 2];
+                    case 5: return [2 /*return*/];
+                }
             });
         });
-    }
-}
+    };
+    GorseUtil.prototype.feed_toggleSecretUser = function (args) {
+        var _a;
+        return __awaiter(this, void 0, void 0, function () {
+            var vibes, status, items, i, item, headers, result;
+            var _this = this;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0:
+                        (_a = this.logger) === null || _a === void 0 ? void 0 : _a.info({ args: args }, "feed_toggleSecretUser");
+                        vibes = args.vibes, status = args.status;
+                        items = [];
+                        for (i = 0; i < vibes.length; i++) {
+                            item = {
+                                itemid: vibes[i]._id,
+                                status: status,
+                                adminstatus: "",
+                            };
+                            items.push(item);
+                        }
+                        headers = { "Content-Type": "application/json" };
+                        return [4 /*yield*/, axios_1.default
+                                .patch(this.feedUrl + "item", items, {
+                                headers: headers,
+                            })
+                                .catch(function (err) {
+                                var _a;
+                                (_a = _this.logger) === null || _a === void 0 ? void 0 : _a.error({ err: err }, "failed to patch vibe status to ".concat(status, " in gorse feed"));
+                            })];
+                    case 1:
+                        result = _b.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    return GorseUtil;
+}());
 exports.default = GorseUtil;
